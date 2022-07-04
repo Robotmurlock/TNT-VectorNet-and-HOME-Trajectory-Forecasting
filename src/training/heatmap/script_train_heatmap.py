d915616a54b5980e53301c6da4012f81985478de
Robotmurlock <momir.adzemovic@gmail.com>
2022-07-04 23:59:41 +0200

    Add (text): Update first two sections and add new sections

--- src/training/heatmap/script_train_heatmap.py
+++ src/training/heatmap/script_train_heatmap.py
@@ -2,8 +2,10 @@ import os
 
 import configparser
 from utils import steps
-from datasets.heatmap_output_dataset import HeatmapOutputRasterScenarioDatasetTorchWrapper
-from architectures.heatmap_output.model import HeatmapModel, PixelFocalLoss
+from datasets.heatmap_dataset import HeatmapOutputRasterScenarioDatasetTorchWrapper
+from architectures.heatmap.model import HeatmapModel, PixelFocalLoss
+import matplotlib.pyplot as plt
+from pathlib import Path
 
 from torch.utils.data import DataLoader
 from torch import optim
@@ -15,9 +17,7 @@ def run(config: configparser.GlobalConfig):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # TODO: Move to config
     logging.info(f'Training on `{device}` device')
 
-    input_path = os.path.join(config.global_path, config.raster.train.input_path)
-
-    dataset = HeatmapOutputRasterScenarioDatasetTorchWrapper(input_path)
+    dataset = HeatmapOutputRasterScenarioDatasetTorchWrapper(config)
     data_loader = DataLoader(dataset, batch_size=16, num_workers=4)
     model = HeatmapModel(
         encoder_input_shape=(10, 224, 224),
@@ -26,11 +26,14 @@ def run(config: configparser.GlobalConfig):
         traj_length=20
     ).to(device)
     criteria = PixelFocalLoss().to(device)
-    optimizer = optim.Adam(params=model.parameters(), lr=1e-3)
-    epochs = 100
+    optimizer = optim.Adam(params=model.parameters(), lr=1e-4)
+    epochs = 10
 
     model.train()
     for epoch in range(1, epochs+1):
+        total_loss = 0.0
+        n_steps = 0
+
         for (raster, trajectory, da_area), heatmap in data_loader:
             optimizer.zero_grad()
 
@@ -38,29 +41,35 @@ def run(config: configparser.GlobalConfig):
             pred_heatmap = model(raster, trajectory)
 
             loss = criteria(pred_heatmap, true_heatmap, da_area)
-            logging.info(f'[Epoch-{epoch}]: loss={loss.detach().item():.4f}.')
             loss.backward()
 
             optimizer.step()
+            total_loss += loss.detach().item()
+            n_steps += 1
 
-    import matplotlib.pyplot as plt
+        logging.info(f'[Epoch-{epoch}]: loss={total_loss:.4f}.')
+
     fig = plt.figure(figsize=(10, 8))
     model.eval()
+    result_path = os.path.join(config.global_path, 'test_data', 'result')
+    Path(result_path).mkdir(parents=True, exist_ok=True)
+    data_loader = DataLoader(dataset, batch_size=1, num_workers=4)
+    index = 0
     for (raster, trajectory, da_area), heatmap in data_loader:
         raster, trajectory, da_area, true_heatmap = raster.to(device), trajectory.to(device), da_area.to(device), heatmap.to(device)
         pred_heatmap = (model(raster, trajectory) * da_area).detach().cpu().numpy()
         true_heatmap = true_heatmap.detach().cpu().numpy()
-        for index in range(pred_heatmap.shape[0]):
-            fig.clf()
-            ax = fig.add_subplot(1, 2, 1)
-            ax.imshow(pred_heatmap[index][0], cmap='YlOrBr', origin='lower')
-            ax.set_title('pred')
+        fig.clf()
+        ax = fig.add_subplot(1, 2, 1)
+        ax.imshow(pred_heatmap[0][0], cmap='YlOrBr', origin='lower')
+        ax.set_title('pred')
 
-            ax = fig.add_subplot(1, 2, 2)
-            ax.imshow(true_heatmap[index][0], cmap='YlOrBr', origin='lower')
-            ax.set_title('true')
-            plt.tight_layout()
-            fig.savefig(os.path.join(config.global_path, 'test_data', 'result', f'heatmap_{index}.png'))
+        ax = fig.add_subplot(1, 2, 2)
+        ax.imshow(true_heatmap[0][0], cmap='YlOrBr', origin='lower')
+        ax.set_title('true')
+        plt.tight_layout()
+        fig.savefig(os.path.join(result_path, f'heatmap_{index}.png'))
+        index += 1
 
 
 if __name__ == '__main__':
